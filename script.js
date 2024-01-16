d0c077ef52ad3478da927ebedd2b62fe145dc5ac
btcftw-dev <124632636+btcftw-dev@users.noreply.github.com>
2024-01-16 16:00:32 -0600

    Add all the accessory divs

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 'use-strict';
 
+///////////////////////////////////////////////////////////
+// Variables
+
 let target;
 let mouseDown = false;
 const container = document.querySelector('.container');
+
+///////////////////////////////////////////////////////////
+// Event Listeners
 
 container.addEventListener('mousedown', e => {
   e.preventDefault();
@@ -18,16 +24,10 @@ container.addEventListener('mouseover', mouseOver);
 
 window.addEventListener('mouseup', e => {
   mouseDown = false;
-  console.log(mouseDown);
 });
 
-// container.addEventListener('mouseup', e => {
-//   mouseDown = false;
-// });
-
-// container.addEventListener('mouseout', e => {
-//   mouseDown = false;
-// });
+///////////////////////////////////////////////////////////
+// Functions
 
 function makeGrid(num) {
   for (let i = 0; i < num; i++) {
@@ -43,47 +43,12 @@ function makeGrid(num) {
 }
 
 function mouseOver(e) {
-  // e.preventDefault();
   target = e.target.classList;
-  console.log(mouseDown, 'status');
-
-  // if (mouseDown === false) {
-  //   container.removeEventListener('mouseover', mouseOver);
-
   if (mouseDown === false) {
-    return console.log('it is false');
+    return;
   } else if (target.contains('column')) {
     target.add('black');
-    console.log('bug');
   }
 }
 
 makeGrid(9);
-
-const column = document.querySelectorAll('.column');
-
-// column.forEach(gridBox => {
-//   gridBox.addEventListener('mousedown', e => {
-//     e.target.classList.add('black');
-
-//     column.forEach(inner => {
-//       inner.addEventListener('mouseover', mouseOver);
-//     });
-//   });
-// });
-
-// window.addEventListener('mouseup', e => {
-//   container.removeEventListener('mouseover', mouseOver);
-//   console.log('mouse up');
-// });
-
-// container.addEventListener('mouseup', e => {
-//   container.removeEventListener('mouseover', mouseOver);
-//   console.log('contaienr mouseup');
-// });
-
-// column.forEach(column => {
-//   container.addEventListener('mouseup', e => {
-//     container.removeEventListener('mouseover', mouseOver);
-//   });
-// })
