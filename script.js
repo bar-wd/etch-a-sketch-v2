4f09b1849617312c0e86b10a7a7554cf09fe6c2b
btcftw-dev <124632636+btcftw-dev@users.noreply.github.com>
2024-01-15 23:03:51 -0600

    Make functioning etch-a-sketch Dragging the mouse now changes all of the boxes.
    Learned the difference between the mouseup,
    mousedown, and click events.
    
    Needed to prevent default behavior on mousedown.
    Default behavior is to drag the object which
    inhibits a functioning etch-a-sketch.

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 'use-strict';
 
+let target;
+let mouseDown = false;
 const container = document.querySelector('.container');
+
+container.addEventListener('mousedown', e => {
+  e.preventDefault();
+  mouseDown = true;
+  target = e.target.classList;
+
+  if (target.contains('column')) {
+    target.add('black');
+  }
+});
+
+container.addEventListener('mouseover', mouseOver);
+
+window.addEventListener('mouseup', e => {
+  mouseDown = false;
+  console.log(mouseDown);
+});
+
+// container.addEventListener('mouseup', e => {
+//   mouseDown = false;
+// });
+
+// container.addEventListener('mouseout', e => {
+//   mouseDown = false;
+// });
 
 function makeGrid(num) {
   for (let i = 0; i < num; i++) {
@@ -15,17 +42,48 @@ function makeGrid(num) {
   }
 }
 
+function mouseOver(e) {
+  // e.preventDefault();
+  target = e.target.classList;
+  console.log(mouseDown, 'status');
+
+  // if (mouseDown === false) {
+  //   container.removeEventListener('mouseover', mouseOver);
+
+  if (mouseDown === false) {
+    return console.log('it is false');
+  } else if (target.contains('column')) {
+    target.add('black');
+    console.log('bug');
+  }
+}
+
 makeGrid(9);
 
-container.addEventListener('click', e => {
-  let targetClassList = e.target.classList;
+const column = document.querySelectorAll('.column');
 
-  if (
-    targetClassList.contains('row') ||
-    targetClassList.contains('container')
-  ) {
-    return;
-  }
+// column.forEach(gridBox => {
+//   gridBox.addEventListener('mousedown', e => {
+//     e.target.classList.add('black');
 
-  targetClassList.add('black');
-});
+//     column.forEach(inner => {
+//       inner.addEventListener('mouseover', mouseOver);
+//     });
+//   });
+// });
+
+// window.addEventListener('mouseup', e => {
+//   container.removeEventListener('mouseover', mouseOver);
+//   console.log('mouse up');
+// });
+
+// container.addEventListener('mouseup', e => {
+//   container.removeEventListener('mouseover', mouseOver);
+//   console.log('contaienr mouseup');
+// });
+
+// column.forEach(column => {
+//   container.addEventListener('mouseup', e => {
+//     container.removeEventListener('mouseover', mouseOver);
+//   });
+// })
